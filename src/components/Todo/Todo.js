eef51db218435084027c7a7393aeaf50fd2440f8
Israfil Hossen <sisrafilss@gmail.com>
2023-01-20 12:30:18 +0600

    todo app initialize

---
+++ src/components/Todo/Todo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Todo = () => {
+    return (
+        <div>
+            <h1 className='h1 text-center mt-5'>Welcome to Simple Todo App</h1>
+        </div>
+    );
+};
+
+export default Todo;
