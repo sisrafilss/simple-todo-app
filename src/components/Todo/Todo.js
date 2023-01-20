73a4f7acd1c38c7225dd7590e32c735cf9de87bc
Israfil Hossen <sisrafilss@gmail.com>
2023-01-20 12:46:58 +0600

    testing h2 added

--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -4,6 +4,7 @@ const Todo = () => {
     return (
         <div>
             <h1 className='h1 text-center mt-5'>Welcome to Simple Todo App</h1>
+            <h2 className='h2 text-center my-4'>This is a Testing Subtitle</h2>
         </div>
     );
 };
